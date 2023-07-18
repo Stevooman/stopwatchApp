282b54b8fd23592605884483382dcc6ecf2b56ce
Steve Oman <stephen_oman145@yahoo.com>
2023-07-18 14:16:15 -0700

    Issue #1 - Rename LapTimes.vue to Buttons.vue for more general reusability

---
+++ src/components/Buttons.vue
@@ -0,0 +1,53 @@
+<template>
+    <button 
+      @click="onClick"
+      >{{ buttonText }}
+    </button>
+    <!-- <button 
+      class="btns pauseBtn"
+      @click="onClick"
+      >Pause</button>
+    <button 
+      class="btns resetBtn"
+      @click="resetTimer"
+      >Reset
+    </button>
+    <button 
+      class="btns lapBtn"
+      @click="logLap"
+      >Lap
+    </button>
+    <button 
+      class="btns viewLapBtn"
+      @click="displayLaps"
+      >{{ viewLapBtnText }}
+    </button> -->
+</template>
+
+
+
+
+
+<script>
+
+  export default {
+    props: {
+      onClick: {
+        required: true,
+        type: Function
+      },
+
+      buttonText: {
+        required: true,
+        type: String
+      }
+    }
+  }
+
+</script>
+
+
+
+
+
+<style></style>
