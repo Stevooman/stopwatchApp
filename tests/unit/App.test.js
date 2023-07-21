2cd6a10f0b5e6908bbe5bd6c51527814837ac738
Steve Oman <stephen_oman145@yahoo.com>
2023-07-21 12:07:03 -0700

    Issue #5 - Add test case for correct lap time display in Laps.vue

--- tests/unit/App.test.js
+++ tests/unit/App.test.js
@@ -1,9 +1,3 @@
 import { mount } from '@vue/test-utils';
 import App from './src/App.vue';
 import { expect } from 'vitest';
-
-const wrapper = mount(App);
-
-test('startTimer method exists', () => {
-  expect(wrapper.startTimer()).toBeDefined();
-})
--- tests/unit/laps.test.js
+++ tests/unit/laps.test.js
@@ -1,15 +1,23 @@
 import { mount } from '@vue/test-utils';
 import Laps from './src/components/Laps.vue';
-import { expect } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
-const wrapper = mount(Laps, {
-  props: {
-    id: 1,
-    displayLap: true,
-    lapTimes: []
-  }
+describe('Laps.vue', () => {
+  test('Renders a lap label and time display for a recorded lap', () => {
+    const lapTimes = [ 
+      { 
+        label: 'Lap 1', time: '05:37:87' 
+      } 
+    ];
+
+    const wrapper = mount(Laps, {
+      props: { 
+        lapTimes: lapTimes,
+        id: 1,
+        displayLap: true
+      },
+    });
+    expect(wrapper.find('.lap').text()).toBe('Lap 1');
+    expect(wrapper.find('.time').text()).toBe('05:37:87');
+  });
 });
-
-it('Lap component prop id contains a number', async () => {
-  expect(wrapper.props().id).toEqual(1);
-})
